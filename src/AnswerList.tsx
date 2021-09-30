979324a9a7ffbdc71b0213a97cb3034d891dcc58
oxenprogrammer <emmy1000okello@gmail.com>
2021-09-30 22:15:31 +0300

    Feat(answer): add list of answers

--- src/AnswerList.tsx
+++ src/AnswerList.tsx
@@ -1,3 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
 export const AnswerList = () => {
-  return (<div>Answer List</div>);
+  interface IAnswer {
+    id: number,
+    content: string,
+    createdAt: string,
+    updatedAt: string,
+  }
+  
+  const [ answers, setAnswers ] = useState<IAnswer[]>([]);
+  const [ listening, setListening ] = useState<boolean>(false);
+
+  useEffect( () => {
+    if (!listening) {
+      const events = new EventSource('http://localhost:4001/api/v1/answers');
+
+      events.onmessage = (event) => {
+        const parsedData = JSON.parse(event.data);
+
+        setAnswers((answers) => answers.concat(parsedData));
+      };
+
+      setListening(true);
+    }
+  }, [listening, answers]);
+
+  return (
+     <div>
+       {
+         answers.map((answer, index) => (
+          <div key={index}>{ answer.content }</div>
+         )
+         )
+       }
+     </div>
+  );
 }
