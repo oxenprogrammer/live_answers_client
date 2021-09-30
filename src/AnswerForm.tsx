bf92a5151342355a5ef5e98301f9f75b6d587f7b
oxenprogrammer <emmy1000okello@gmail.com>
2021-09-30 22:11:39 +0300

    Feat(question): update form to textarea

--- src/AnswerForm.tsx
+++ src/AnswerForm.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, useState } from 'react';
 
 import axios from 'axios';
 
-const AnswerForm = () => {
+export const AnswerForm = () => {
 
   const [answer, setAnswer] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -20,7 +20,7 @@ const AnswerForm = () => {
   const sterilizedAnswer = answer.replace(/[^a-zA-Z]/g, "").toLowerCase();
 
 
-  const handleChange = ({ target: { value }}: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = ({ target: { value }}: ChangeEvent<HTMLTextAreaElement>) => {
     setAnswer(value);
   }
 
@@ -34,7 +34,7 @@ const AnswerForm = () => {
     
     setIsLoading(true);
     const data = { content: answer };
-    const response = await axios.post('http://127.0.0.1:4001/answers', data);
+    const response = await axios.post('http://127.0.0.1:4001/api/v1/answers', data);
     console.log(response.status);
     if (response.status !== 200) {
       setServerResponse('An error occurred submitting to the server');
@@ -51,15 +51,16 @@ const AnswerForm = () => {
     {serverResponse ? <div>{ serverResponse }</div> : ''}
     <form onSubmit={handleSubmit}
       >
-        <input
-          type="text"
-          onChange={handleChange}
-          value={answer}
-        />
-        <input type="submit" disabled={isLoading} value="Add Answer" />
+        <div>
+          <textarea
+            onChange={handleChange}
+            value={answer}
+          ></textarea>
+        </div>
+        <div>
+          <input type="submit" disabled={isLoading} value="Add Answer" />
+        </div>
       </form>
   </React.Fragment>
   )
 }
-
-export default AnswerForm;
